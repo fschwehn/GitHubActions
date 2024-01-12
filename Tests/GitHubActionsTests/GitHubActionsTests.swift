89fde58c53aa79ce4b678f7543ce674df18f2788
Florian Schwehn <scf1lb@bosch.com>
2024-01-12 12:13:55 +0100

    test

--- Tests/GitHubActionsTests/GitHubActionsTests.swift
+++ Tests/GitHubActionsTests/GitHubActionsTests.swift
@@ -6,9 +6,9 @@ final class GitHubActionsTests: XCTestCase {
         XCTAssertEqual(theAnswer(), 42)
     }
 
-//    func test_fails() throws {
-//        XCTAssertEqual(theAnswer(), 23)
-//    }
+    func test_fails() throws {
+        XCTAssertEqual(theAnswer(), 23)
+    }
 
     func test_2() throws {
         XCTAssertTrue(true)
